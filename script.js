c80acbfc112f9cc90e947da6f719c7e3b7fcc8fe
ZKala <46380288+ZKala@users.noreply.github.com>
2022-09-09 05:53:16 -0700

    Responsiveness Update
    Screen Adjustments for mobile

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+document.querySelector('.menu').addEventListener('click', () => {
+   document.querySelectorAll('.target').forEach((item) =>{
+    item.classList.toggle('change');
+   })
+})
+
 const images = document.querySelectorAll('section-1-images')
 let i = 1;
 
